77180941740465d8bacf43bca587c0869ad1b455
Srijit Bera <berasrijit02@gmail.com>
2023-03-07 11:00:49 +0530

    Add files via upload

---
+++ Basics in Python/5_comment_escape.py
@@ -0,0 +1,14 @@
+# i am a comment in your program
+
+print("Srijit is a god boy\n who loves to eat food")
+'''
+ i am 
+    multiline 
+    comment
+'''
+
+# Escape Sequence
+print("I am srijit bera \"Srijit\" ")
+
+print("Srijit ", 45, 5, sep="~",end=" ")
+print("Kamalaika")
---
+++ Basics in Python/6_Variable.py
@@ -0,0 +1,14 @@
+a =10
+b=complex(8,2)
+print("sum is",(a+b))
+a=10
+c=True
+d=9.6
+e="Srijit"
+f=None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
