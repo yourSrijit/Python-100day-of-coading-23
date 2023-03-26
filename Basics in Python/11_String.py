9bb93dc4ed661cb8ab95b59a1052c2cead92120c
Srijit Bera <berasrijit02@gmail.com>
2023-03-26 20:24:04 +0530

    Add files via upload

---
+++ Basics in Python/11_String.py
@@ -0,0 +1,11 @@
+string ="i am srijit bera"
+print(string)
+String =''' I am srijit bea
+                    i love to eat food
+                             love to travel'''
+print(String)
+print(string[0:6])
+print(string[:4])
+print(string[-1:-4])
+#len(string)-1 :len(string)-4
+print(string[-4:-1])
